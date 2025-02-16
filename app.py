1ae3892ad7c79675f7051f92603656827055c653
Sukalyan04 <158624292+Sukalyan04@users.noreply.github.com>
2025-02-16 02:08:42 -0600

    Audio transcript added

--- app.py
+++ app.py
@@ -1,50 +1,61 @@
 import streamlit as st
 from dotenv import load_dotenv
-load_dotenv() ##load all the environment variables
+load_dotenv()
 import os
 import google.generativeai as genai
-
 from youtube_transcript_api import YouTubeTranscriptApi
+from gtts import gTTS
+from io import BytesIO
+import base64
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 prompt = "You are a YouTube video summarizer. You'll be taking the transcript text and summarizing the entire video and providing the important summary in bullet formatting within 200 or 250 words. Please provide the summary of the text given here : "
 
-
-##getting the transcript data from YT videos
 def extract_transcript_details(youtube_video_url):
     try:
         video_id = youtube_video_url.split("=")[1]
-        print(video_id)
         transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
-
-        transcript = ""
-        for i in transcript_text:
-            transcript += " " +i["text"]
+        transcript = " ".join([i["text"] for i in transcript_text])
         return transcript
     except Exception as e:
         raise e
 
-#getting the summary based on prompt from Google Gemini Pro
 def generate_gemini_content(transcript_text, prompt):
     model = genai.GenerativeModel("gemini-pro")
-    response = model.generate_content(prompt+transcript_text)
+    response = model.generate_content(prompt + transcript_text)
     return response.text
 
+def text_to_speech(text, language='en'):
+    tts = gTTS(text=text, lang=language, slow=False)
+    fp = BytesIO()
+    tts.write_to_fp(fp)
+    fp.seek(0)
+    return fp
 
-st.title("Youtube Transcript to Detailed Notes COnverter")
-youtube_link = st.text_input("Enter Youtube Video LInk:")
+st.title("YouTube Transcript to Detailed Notes Converter")
+youtube_link = st.text_input("Enter YouTube Video Link:")
 
 if youtube_link:
     video_id = youtube_link.split("=")[1]
-    print(video_id)
-
     st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
 
 if st.button("Get Detailed Notes"):
     transcript_text = extract_transcript_details(youtube_link)
 
     if transcript_text:
-        summary=generate_gemini_content(transcript_text, prompt)
-        st.markdown("##Detailed Notes:")
+        summary = generate_gemini_content(transcript_text, prompt)
+        st.markdown("## Detailed Notes:")
         st.write(summary)
+
+        # Convert summary to speech
+        audio_fp = text_to_speech(summary)
+        st.audio(audio_fp, format='audio/mp3')
+
+        # Provide download button for audio
+        st.download_button(
+            label="Download Audio Summary",
+            data=audio_fp,
+            file_name="summary_audio.mp3",
+            mime="audio/mp3"
+        )
